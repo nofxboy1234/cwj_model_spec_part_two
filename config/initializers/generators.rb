bc75aa0866615f15e823553a9f9569be9e832b39
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-25 09:38:58 +0200

    Fix styling

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,4 +1,4 @@
-            Rails.application.config.generators do |g|
+Rails.application.config.generators do |g|
   g.test_framework :rspec,
                    fixtures: false,
                    view_specs: false,
