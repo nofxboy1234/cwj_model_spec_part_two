226a46cd2befc8dfd935603722c1880c02496c0d
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-25 11:05:44 +0200

    Make test pass

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -2,6 +2,10 @@ class PhoneNumber < ApplicationRecord
   before_validation :strip_non_numeric_from_value
 
   def strip_non_numeric_from_value
+    value = 19
     self.value = self.value.gsub(/\D/, '')
+                     .split('')
+                     .last(10)
+                     .join
   end
 end
