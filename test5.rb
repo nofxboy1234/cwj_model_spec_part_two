6fd1fc391864120c63bd6b108eb4b989c5310571
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-27 12:15:16 +0200

    Update test5.rb

--- test5.rb
+++ test5.rb
@@ -1,7 +1,7 @@
 class User
   def create_method(method_name, &block)
     method_definition = proc do |*args, &value_block|
-      puts args
+      p args
       if value_block
         instance_variable_set("@#{method_name}", value_block.call)
       else
@@ -15,13 +15,12 @@ class User
   end
 
   def method_missing(method_name, *_args, &block)
+    p 'method_missing'
     create_method(method_name, &block)
   end
 end
 
 user1 = User.new
-user1.first_name { 'John' }
-p user1.first_name
-user1.first_name { 'Johnny' }
-p user1.first_name
-p user1.first_name('a', 'b', 'c') { 'Dylan' }
+p user1.first_name { 'a' }
+p user1.first_name { 'b' }
+p user1.first_name('x', 'y', 'z') { 'c' }
