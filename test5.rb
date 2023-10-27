3314d29172d3f5117190978d590caa42556c0b10
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-27 14:31:06 +0200

    update

--- test5.rb
+++ test5.rb
@@ -1,15 +1,21 @@
 class User
   def create_method(method_name, &block)
+    puts 'creating method'
+
     method_definition = proc do |*_args, &value_block|
+      puts "\ncalling #{method_name}"
+
       if value_block
+        puts 'with a block'
         instance_variable_set("@#{method_name}", value_block.call)
       else
+        puts 'without a block'
         instance_variable_get("@#{method_name}")
       end
     end
 
     self.class.define_method(method_name, method_definition)
-    send(method_name, &block)
+    # send(method_name, &block)
   end
 
   def method_missing(method_name, *_args, &block)
@@ -18,7 +24,7 @@ class User
 end
 
 user1 = User.new
-p(user1.first_name { 'a' })
-p user1.first_name('x', 'y', 'z')
-p(user1.first_name { 'b' })
-p user1.first_name('x', 'y', 'z') { 'c' }
+user1.first_name
+
+user1.first_name
+user1.first_name { 'a' }
