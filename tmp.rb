ad968d3c78b8e8f5d766ccf47a86173e6eb781ba
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-31 22:30:33 +0200

    update

--- tmp.rb
+++ tmp.rb
@@ -1,13 +1,24 @@
 describe 'attributes' do
   let!(:time_block_type) { create(:time_block_type, code: 'APPOINTMENT') }
+  let!(:stylist) { create(:stylist) }
 
   let(:appointment) do
     AppointmentFactory.find_or_create(
-      appointment: { time_block_type_code: 'APPOINTMENT' }
+      appointment: {
+        time_block_type_code: 'APPOINTMENT',
+        notes: 'always late',
+        stylist_id: stylist.id,
+        is_cancelled: 'true',
+        tip: '100'
+      }
     )
   end
 
-  it 'sets a time block type' do
+  it 'sets the proper attributes' do
     expect(appointment.time_block_type).to eq(time_block_type)
+    expect(appointment.notes).to eq('always late')
+    expect(appointment.stylist).to eq(stylist)
+    expect(appointment.is_cancelled).to be true
+    expect(appointment.tip).to eq(100)
   end
 end
