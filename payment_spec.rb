5d493031ffe6661f5268ab3a72b69ecab404251f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-01 11:47:10 +0200

    Replace PaymentGateway instance with a TestDouble->Stub

--- payment_spec.rb
+++ payment_spec.rb
@@ -4,7 +4,9 @@ require_relative 'logger'
 
 describe Payment do
   it 'records the payment' do
-    payment_gateway = PaymentGateway.new
+    payment_gateway = double()
+    allow(payment_gateway).to receive(:charge).and_return(payment_id: 1234)
+    
     logger = Logger.new
 
     payment = Payment.new(payment_gateway, logger)
