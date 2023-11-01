c5e8bda96b67458240bc14787558c120fa79cc14
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-01 11:33:11 +0200

    Print total_cents charged

--- payment_gateway.rb
+++ payment_gateway.rb
@@ -1,5 +1,6 @@
 class PaymentGateway
   def charge(total_cents)
+    puts "Charged credit card for #{total_cents}"
     puts "THIS HITS THE PRODUCTION API AND ALTERS PRODUCTION DATA. THAT'S BAD!"
 
     { payment_id: rand(1000) }
