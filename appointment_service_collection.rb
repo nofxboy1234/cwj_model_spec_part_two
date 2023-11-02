2a52c6324e6a83309ee82e85bc2ab2f877beed30
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-02 11:03:35 +0200

    Uncomment full body of the to_h method

--- appointment_service_collection.rb
+++ appointment_service_collection.rb
@@ -5,12 +5,12 @@ class AppointmentServiceCollection
 
   def to_h
     @appointment_services.map { |item|
-      #item.serializable_hash.merge(
-        #"price" => number_with_precision(item.price, :precision => 2),
-        #"label" => item.service.name,
-        #"item_id" => item.service.id,
-        #"type" => "service"
-      #)
+      item.serializable_hash.merge(
+        "price" => number_with_precision(item.price, :precision => 2),
+        "label" => item.service.name,
+        "item_id" => item.service.id,
+        "type" => "service"
+      )
     }
   end
 end
