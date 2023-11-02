ede4f372f17c6d784e1ed8079c5afbe750780a5f
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-02 17:45:11 +0200

    Set Capybara.default_driver = :selenium_chrome

--- spec/system/npi_searches_spec.rb
+++ spec/system/npi_searches_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 RSpec.describe "NPISearches", type: :system do
   it 'shows the physician NPI number' do
     visit new_npi_search_path
+    sleep(2)
     fill_in 'first_name', with: 'joel'
+    sleep(2)
     fill_in 'last_name', with: 'fuhrman'
+    sleep(2)
     click_on 'Search'
+    sleep(2)
 
     # 1386765287 is the NPI code for Dr. Joel Fuhrman
     expect(page).to have_content('1386765287')
