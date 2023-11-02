dcdc967f7e878b68ea6053640c9d80a3f94a4a0c
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-02 11:58:47 +0200

    Add test for #number_is_right_length

--- credit_card_spec.rb
+++ credit_card_spec.rb
@@ -10,4 +10,15 @@ describe CreditCard do
       end
     end
   end
+
+  describe '#number_is_right_length?' do
+    context 'no spaces' do
+      context 'right length' do
+        it 'returns true' do
+          cc = CreditCard.new('3843111122223333', '02/17', 'Visa')
+          expect(cc.number_is_right_length?).to be true
+        end
+      end
+    end
+  end
 end
