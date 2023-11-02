fffa247a3082f65d72217550dd72f11876daded6
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-02 21:20:04 +0200

    Use VCR and WebMock in system test by recording to a cassette yml file

--- spec/system/npi_searches_spec.rb
+++ spec/system/npi_searches_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "NPISearches", type: :system do
+RSpec.describe 'NPISearches', type: :system do
   it 'shows the physician NPI number' do
-    visit new_npi_search_path
-    # sleep(1)
-    fill_in 'first_name', with: 'joel'
-    # sleep(1)
-    fill_in 'last_name', with: 'fuhrman'
-    # sleep(1)
-    click_on 'Search'
-    # sleep(2)
+    VCR.use_cassette 'npi_search' do
+      visit new_npi_search_path
+      # sleep(1)
+      fill_in 'first_name', with: 'joel'
+      # sleep(1)
+      fill_in 'last_name', with: 'fuhrman'
+      # sleep(1)
+      click_on 'Search'
+      # sleep(2)
 
-    # 1386765287 is the NPI code for Dr. Joel Fuhrman
-    expect(page).to have_content('1386765287')
+      # 1386765287 is the NPI code for Dr. Joel Fuhrman
+      expect(page).to have_content('1386765287')
+    end
   end
 end
