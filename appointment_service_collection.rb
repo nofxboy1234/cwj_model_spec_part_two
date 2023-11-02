e0208a224e0cd0bd07723a987371b466331fabda
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-02 11:22:25 +0200

    With test preserving the original functionality, clean up and refactor code

--- appointment_service_collection.rb
+++ appointment_service_collection.rb
@@ -1,18 +1,25 @@
 class AppointmentServiceCollection
   include ActionView::Helpers::NumberHelper
+  ITEM_TYPE = 'service'
 
   def initialize(appointment_services)
     @appointment_services = appointment_services
   end
 
   def to_h
-    @appointment_services.map { |item|
-      item.serializable_hash.merge(
-        "price" => number_with_precision(item.price, :precision => 2),
-        "label" => item.service.name,
-        "item_id" => item.service.id,
-        "type" => "service"
-      )
+    @appointment_services.map do |appointment_service|
+      appointment_service.serializable_hash.merge(extra_attributes(appointment_service))
+    end
+  end
+
+  private
+
+  def extra_attributes(appointment_service)
+    {
+      'price'   => number_with_precision(appointment_service.price, precision: 2),
+      'label'   => appointment_service.service.name,
+      'item_id' => appointment_service.service.id,
+      'type'    => ITEM_TYPE
     }
   end
 end
