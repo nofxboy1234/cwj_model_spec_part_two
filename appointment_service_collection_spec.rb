e9553e08c8acf77a98271350486106834893da42
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-02 11:19:09 +0200

    Make the test setup match assertion values

--- appointment_service_collection_spec.rb
+++ appointment_service_collection_spec.rb
@@ -3,13 +3,18 @@ require 'spec_helper'
 describe AppointmentServiceCollection do
   describe '#to_h' do
     it 'works' do
-      appointment_service = create(:appointment_service)
+      appointment_service = create(
+        :appointment_service,
+        price: 30,
+        service: create(:service, name: 'Mens Haircut')
+      )
+
       collection = AppointmentServiceCollection.new([appointment_service])
 
       item = collection.to_h.first
       expect(item['price']).to eq('30.00')
       expect(item['label']).to eq('Mens Haircut')
-      expect(item['item_id']).to eq(appointment_service.item.id)
+      expect(item['item_id']).to eq(appointment_service.service.id)
       expect(item['type']).to eq('service')
     end
   end
