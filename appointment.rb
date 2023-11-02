625476b6e615cdacc6b74fa4af8cbc4bb903043a
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-02 10:49:42 +0200

    Move body of Appointment#services_with_info to new AppointmentServiceCollection class

--- appointment.rb
+++ appointment.rb
@@ -1,13 +1,6 @@
 class Appointment < ActiveRecord::Base
   def services_with_info
-    appointment_services.collect { |item|
-      item.serializable_hash.merge(
-        "price" => number_with_precision(item.price, :precision => 2),
-        "label" => item.service.name,
-        "item_id" => item.service.id,
-        "type" => "service"
-      )
-    }
+
   end
 
   def products_with_info
---
+++ appointment_service_collection.rb
@@ -0,0 +1,16 @@
+class AppointmentServiceCollection
+  def initialize(appointment_services)
+    @appointment_services = appointment_services
+  end
+
+  def to_h
+    @appointment_services.to_h { |item|
+      item.serializable_hash.merge(
+        "price" => number_with_precision(item.price, :precision => 2),
+        "label" => item.service.name,
+        "item_id" => item.service.id,
+        "type" => "service"
+      )
+    }
+  end
+end
