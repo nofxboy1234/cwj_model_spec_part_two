d2c9535a8d886441c1b9867823c15de4405177de
Dylan Palmboom <nofxboy1234@gmail.com>
2023-11-03 11:09:14 +0200

    Add original_code.rb

---
+++ original_code.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+class UserTest < ActiveSupport::TestCase
+
+  let(:user) { FactoryBot.build :user}
+
+  it "must be valid" do
+    value(user).must_be :valid?
+  end
+
+  it "can be saved" do
+    assert_difference "User.count", 1 do
+      user.save
+    end
+    assert user.persisted?
+  end
+
+  describe "won't be valid" do
+    it "with an duplicated email" do
+      user.save
+      user2 = FactoryBot.build :user
+      user2.wont_be :valid?
+      user2.errors.count.must_equal 1
+      user2.errors[:email].must_equal ["has already been taken"]
+    end
+
+    it "without an email" do
+      user.email = nil
+      value(user).wont_be :valid?
+      user.errors[:email].first.must_equal "can't be blank"
+    end
+
+    it "with an falsy email" do
+      user.email = "thisis@falsemail"
+      value(user).wont_be :valid?
+      user.errors[:email].first.must_equal "is invalid"
+    end
+
+    it "with an email without a mx-record" do
+      user.email = "hi@thisdomainwillneverexistnorhaveamxrecord.com"
+      value(user).wont_be :valid?
+      user.errors[:email].first.must_equal "is invalid"
+    end
+
+    it "with an email that is on our blacklist" do
+      user.email = "test@trashmail.com"
+      value(user).wont_be :valid?
+      user.errors[:email].first.must_equal "is a blacklisted email provider"
+    end
+  end
+end
